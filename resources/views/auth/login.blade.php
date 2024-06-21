0a9a162b4c233b49f97b062537c3f89cc29bf0e9
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-06-21 08:56:19 +0200

    "Updated login and register views to add cancel buttons and adjust layout"

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,15 @@
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
+<div class="mt-5">
+    <x-header>Login</x-header>
 
-<x-header>Login</x-header>
+    <x-form method="POST" action="/login">
+        <x-input label="Email" name="email" type="email"></x-input>
+        <x-input label="Password" name="password" type="password"></x-input>
 
-<x-form method="POST" action="/login">
-    <x-input label="Email" name="email" type="email"></x-input>
-    <x-input label="Password" name="password" type="password"></x-input>
-
-    <x-button>Sign In</x-button>
-</x-form>
+        <div class="w-50 d-flex justify-content-between">
+            <x-button>Sign In</x-button>
+            <x-button class="btn-secondary"><a href="/" class="btn-primary text-deco">Cancel</a></x-button>
+        </div>
+    </x-form>
+</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,17 @@
 @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-<x-header>Register</x-header>
 
-<x-form method="POST" action="/register">
-    <x-input label="Email" name="email" type="email"></x-input>
-    <x-input label="Password" name="password" type="password"></x-input>
-    <x-input label="Password Confirmation" name="password_confirmation" type="password"></x-input>
+<div class="mt-5">
+    <x-header>Register</x-header>
 
-    <x-button>Sign Up</x-button>
-</x-form>
+    <x-form method="POST" action="/register">
+        <x-input label="Email" name="email" type="email"></x-input>
+        <x-input label="Password" name="password" type="password"></x-input>
+        <x-input label="Password Confirmation" name="password_confirmation" type="password"></x-input>
+
+        <div class="w-50 d-flex justify-content-between">
+            <x-button>Sign Up</x-button>
+            <x-button class="btn-secondary"><a href="/" class="btn-primary text-deco">Cancel</a></x-button>
+        </div>
+    </x-form>
+</div>
